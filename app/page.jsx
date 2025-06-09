4300f4565526478430430e6e83eddfa1d5b3b69d
SURAJSCHAUHAN <chauhansurajsingh.2020cs@technonjr.org>
2025-06-09 23:37:26 +0530

    changes

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="h-100 flex justify-center items-center text-5xl font-mono">
+      Lets Play!
+    </div>
+  );
+};
+
+export default page;
