fef63ad544b1cb5ec452ffd4e21bb7dc5eb43c15
SURAJSCHAUHAN <chauhansurajsingh.2020cs@technonjr.org>
2025-06-16 22:13:30 +0530

    feature added

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
 
 const page = () => {
+  const [id, setId] = useState("");
+
   return (
-    <div className="h-100 flex justify-center items-center text-5xl font-mono">
+    <div className="h-[100vh] flex flex-col gap-20 justify-center items-center text-5xl font-mono">
       Lets Play!
+      <div className="flex flex-col gap-5 items-center">
+        <input
+          className="bg-gray-900 rounded-2xl text-center"
+          type="text"
+          onChange={(e) => setId(e.target.value)}
+        />
+        <Link href={`https://zerocross.vercel.app/${id}`}>GO</Link>
+      </div>
     </div>
   );
 };
